88f986650fe604d2903473ddb7e107b721565aea
oligo <oligo83@gmail.com>
2025-03-03 20:51:41 +0700

    fix: ensuring consistent dependencies again

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
-import Bowman from './js/bowman.js';
-import Swordsman from './js/swordsman.js';
-import Magician from './js/magician.js';
-import Daemon from './js/daemon.js';
-import Undead from './js/undead.js';
-import Zombie from './js/zombie.js';
+import Character from './js/character.js';
 
-const bowman = new Bowman('Леголас');
-bowman.damage(9001);
-const swordsman = new Swordsman('Арагорн');
-swordsman.damage(9001);
-const magician = new Magician('Гэндальф');
-magician.damage(9001);
-const daemon = new Daemon('Балрог');
-daemon.damage(9001);
-const undead = new Undead('Назгул');
-undead.damage(9001);
-const zombie = new Zombie('Роб');
-zombie.damage(9001);
+const character = new Character('Леголас', 'Bowman');
+
+console.log(character.name);
